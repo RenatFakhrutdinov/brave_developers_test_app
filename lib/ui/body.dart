00cfc40eda07feebe274cf15514e9558015eaaed
Renat <rfakhrutdinov1988@gmail.com>
2020-02-26 21:39:39 +0300

    add LayoutBuilder to footer

--- lib/ui/body.dart
+++ lib/ui/body.dart
@@ -2,7 +2,6 @@ import 'package:brave_developers_test_app/res/strings.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatefulWidget {
-
   @override
   _BodyState createState() => _BodyState();
 }
@@ -10,13 +9,8 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        if (constraints.maxWidth < 600) {
-          return Center(child: Text(Strings.title));
-        } else
-          return Center(child: Text(Strings.title));
-      },
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      return Center(child: Text(Strings.title));
+    });
   }
 }
--- lib/ui/footer.dart
+++ lib/ui/footer.dart
@@ -5,52 +5,60 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 180,
-      child: Padding(
-        padding: EdgeInsets.all(24.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text("Contacts"),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 24),
-                    child: Row(
+    return LayoutBuilder(builder: (context, constraints) {
+      print('${constraints.constrainWidth()}');
+      return Container(
+        height: 180,
+        child: Padding(
+          padding: EdgeInsets.all(24.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Contacts"),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 24),
+                      child: Row(
+                        children: <Widget>[
+                          Image.network('assets/github.png'),
+                          SizedBox(width: 8),
+                          Text("My Github")
+                        ],
+                      ),
+                    ),
+                    Row(
                       children: <Widget>[
-                        Image.network('assets/github.png'),
+                        Icon(
+                          Icons.mail,
+                          size: 32,
+                        ),
                         SizedBox(width: 8),
-                        Text("My Github")
+                        InkWell(
+                            onTap: () {},
+                            child:
+                                Text('renat.fakhrutdinov@bravedevelopers.com'))
                       ],
                     ),
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Icon(
-                        Icons.mail,
-                        size: 32,
+                  ],
+                ),
+              ),
+              constraints.constrainWidth() > 650
+                  ? Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Text(Strings.title),
+                        ],
                       ),
-                      SizedBox(width: 8),
-                      Text('renat.fakhrutdinov@bravedevelopers.com')
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(Strings.title),
-                ],
-              ),
-            ),
-          ],
+                    )
+                  : SizedBox.shrink(),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
