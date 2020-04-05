f0e9eb6a5d61dd94b22304a2e2a3fb1c8c9b4ee2
Renat <rfakhrutdinov1988@gmail.com>
2020-04-05 22:53:35 +0300

    change email

--- lib/res/strings.dart
+++ lib/res/strings.dart
@@ -2,7 +2,7 @@ class Strings {
   static const String title = "Web терминал оплаты";
   static const String contacts = "Contacts";
   static const String myGitHub = "My Github";
-  static const String mailOfAuthor = "renat.fakhrutdinov@bravedevelopers.com";
+  static const String mailOfAuthor = "rfakhrutdinov1988@gmail.com";
   static const String mailUrl =
       "mailto:renat.fakhrutdinov@bravedevelopers.com?subject=Mail from web test app&body= ";
   static const String gitHubUrl = 'https://github.com/RenatFakhrutdinov';
